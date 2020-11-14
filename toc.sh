ddfe3d2fb8886d269f2c9900a9d5d6132606e05a
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-11-14 19:42:13 +0100

    update button

--- toc.sh
+++ toc.sh
@@ -14,8 +14,3 @@ make_toc antipatterns
 echo -e "\n\n## Patterns\n"
 
 make_toc patterns
-
-cat << EOM
-
-<script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="hapytex" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can now buy me a coffee!" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
-EOM
