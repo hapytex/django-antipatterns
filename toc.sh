e375c2826bdd8921e3007f5804b68c38d130f83e
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-02-24 12:35:47 +0100

    two column layout

--- toc.sh
+++ toc.sh
@@ -4,7 +4,9 @@ sep=$(echo -e ". \n")
 
 function make_toc() {
   fl=$(ls $1/*.md)
+  echo '<div class="columns">'
   head -q -n 1 $fl | cut -c 2- | paste -d ':' - <(echo "$fl") | sed -E 's/^.(.*)[:](.*)[.]md/ [\1](\2.html)/g' | paste -d '.' <(seq $(wc -l <<<"$fl")) -
+  echo '</div>'
 }
 
 echo -e "% Django (anti)patterns\n"
