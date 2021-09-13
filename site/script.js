da841ce3c26eaa7357006235cadaf044691851e3
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-09-13 20:52:41 +0200

    copy content to the clipboard

--- site/script.js
+++ site/script.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function($){
 	var copyid = 0;
+  function copyItem(event) {
+    navigator.clipboard.writeText($(this).siblings('pre:first').text());
+    /* console.log(self.parent()); */
+    /* document.getElementById("myInput").textContent;*/
+  };
 	$('pre').each(function(){
 		copyid++;
 		$(this).attr( 'data-copyid', copyid).wrap( '<div class="pre-wrapper"/>');
 		$(this).parent().css( 'margin', $(this).css( 'margin') );
 		$('<button class="copy-snippet"><i class="far fa-clipboard"></i></button>').insertAfter( $(this) ).data( 'copytarget',copyid );
 	});
+  $('div .pre-wrapper button.copy-snippet').click(copyItem);
 });
