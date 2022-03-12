8149c65da48b62901645c6e0aa6f35ad9ef85ef8
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-03-12 19:46:53 +0100

    add rewrite to the write file process

--- rewrite.sh
+++ rewrite.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sed -E 's# \[([A-Za-z0-9.\-]+)\]\]\(#&nbsp;<sup>[\1]](#g'  # rewrite references to Wikipedia-style
+if [ "$#" -lt '1' ]; then
+  echo &>2 'Should contain one parameter: the file to modify'
+  exit 1
+fi
+
+file="$1"
+
+sed -i -E 's# \[([A-Za-z0-9.\-]+)\]\]\(#&nbsp;<sup>[\1]](#g' "$file"  # rewrite references to Wikipedia-style
